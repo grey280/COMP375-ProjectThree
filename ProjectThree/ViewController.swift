c319cfad04e1ab6814b992517a0e90a7265afbf5
Grey Patterson <greypatterson@gmail.com>
2017-04-12 10:58:40 -0700

    Initial Commit

---
+++ ProjectThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectThree
+//
+//  Created by Grey Patterson on 2017-04-12.
+//  Copyright © 2017 Grey Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
