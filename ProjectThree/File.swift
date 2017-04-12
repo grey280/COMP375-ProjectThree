0b580670096b867f9a7c42406892501df06c98be
Grey Patterson <greypatterson@gmail.com>
2017-04-12 11:07:21 -0700

    Build model

---
+++ ProjectThree/File.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  ProjectThree
+//
+//  Created by Grey Patterson on 2017-04-12.
+//  Copyright © 2017 Grey Patterson. All rights reserved.
+//
+
+import Foundation
+
+class File {
+    var title: String
+    var shortDescription: String
+    var URL: NSURL
+    
+    convenience init(title: String, URL: NSURL){
+        self.init(title: title, description: "", URL: URL)
+    }
+    
+    init(title: String, description: String, URL: NSURL){
+        self.title = title
+        self.shortDescription = description
+        self.URL = URL
+    }
+}
