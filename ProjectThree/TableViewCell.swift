885ae59b4d4630a12552e0f0f2f0cbd941f6d774
Grey Patterson <greypatterson@gmail.com>
2017-04-19 11:34:54 -0700

    Add a swipe to switch between description and URL

--- ProjectThree/TableViewCell.swift
+++ ProjectThree/TableViewCell.swift
@@ -18,10 +18,25 @@ class TableViewCell: UITableViewCell {
     }
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    private var showingURL = false
 
+    func swapDisplay(){
+        if(showingURL){
+            showingURL=false
+            descriptionLabel.text = file.URL.absoluteString ?? ""
+        }else{
+            showingURL = true
+            descriptionLabel.text = file.description
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swapDisplay))
+        swipeRecognizer.direction = .right
+        self.addGestureRecognizer(swipeRecognizer)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
