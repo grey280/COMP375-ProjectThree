4fa6805e73c3e2649f7c828a9fe54eb3e2c4a2da
Grey Patterson <greypatterson@gmail.com>
2017-04-19 11:05:03 -0700

    Remove unnecessary override

--- ProjectThree/TableViewController.swift
+++ ProjectThree/TableViewController.swift
@@ -149,14 +149,6 @@ class TableViewController: UITableViewController {
     */
 
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
