fd0160643b97cd60011f061758cba34476cecec7
Grey Patterson <greypatterson@gmail.com>
2017-04-24 11:39:24 -0700

    I'm gonna fight the person who told Xcode to try to map UIWebView.loadRequest to WKWebView.load because they are NOT AT ALL the same

--- ProjectThree/FileViewController.swift
+++ ProjectThree/FileViewController.swift
@@ -12,14 +12,30 @@ class FileViewController: UIViewController {
     
     weak var file: File!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = file.title
         
+        guard let fileType = file.URL.pathExtension else{
+            // crash, I guess?
+            return
+        }
+        switch fileType {
+        case "jpg", "jpeg", "png", "gif": // handle as an image
+            webView.removeFromSuperview()
+            // TODO: finish this
+        default: // handle as a URL
+            imageView.removeFromSuperview()
+            scrollView.removeFromSuperview()
+            let request = NSURLRequest(url: file.URL as URL)
+            webView.load(request as URLRequest, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: URL("this doesn't make sense"))
+            // TODO: finish this
+        }
         
-        // TODO: load either a page or an image here
     }
 
     override func didReceiveMemoryWarning() {
